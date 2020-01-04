be21e75f361d1976164677d0dd51517e6b7ea090
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2020-01-04 13:16:24 -0800

    Updated

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 ﻿#Remove-Item –path ./ –recurse
 & java -jar swagger-codegen-cli-2.4.11.jar generate -i https://api.cloudmersive.com/swagger/api/validate -l cpprest -c packageconfig.json
-$extrasetup = (Get-Content ./extrasetup.py) -join "`n"
-Write-Host $extrasetup
+# $extrasetup = (Get-Content ./extrasetup.py) -join "`n"
+# Write-Host $extrasetup
 # (Get-Content ./setup.py).replace('# http://pypi.python.org/pypi/setuptools', $extrasetup) | Set-Content ./setup.py
 # (Get-Content ./setup.py).replace('"""\', "long_description,`n    long_description_content_type='text/markdown'") | Set-Content ./setup.py
 # (Get-Content ./setup.py).replace('The validation APIs help you validate data. Check if an E-mail address is real. Check if a domain is real. Check up on an IP address, and even where it is located. All this and much more is available in the validation API.  # noqa: E501', '') | Set-Content ./setup.py
